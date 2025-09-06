e22538d18ccba6b0d1acc76575c03c3445c3d9b1
Nanyangu-ommy <onanseta@byupathway.edu>
2025-09-05 18:22:17 -1000

    finished exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101); // random number between 1 and 100
+
+        Console.WriteLine("I'm thinking of a number between 1 and 100. Can you guess it?");
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != magic)
+        {
+            if (guess < magic)
+                Console.WriteLine("Higher");
+            else
+                Console.WriteLine("Lower");
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
