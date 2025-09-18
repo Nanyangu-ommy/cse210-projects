91d3c77c3bb0828e86b1735079bad9bf4f6ba90d
Nanyangu-ommy <onanseta@byupathway.edu>
2025-09-18 04:53:04 -1000

    Fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        
+        Fraction f1 = new Fraction();          
+        Fraction f2 = new Fraction(5);         
+        Fraction f3 = new Fraction(3, 4);      
+        Fraction f4 = new Fraction(1, 3);      
+
+        
+        Console.WriteLine(f1.GetFractionString());   
+        Console.WriteLine(f1.GetDecimalValue());    
+
+        Console.WriteLine(f2.GetFractionString());   
+        Console.WriteLine(f2.GetDecimalValue());     
+
+        Console.WriteLine(f3.GetFractionString());   
+        Console.WriteLine(f3.GetDecimalValue());    
+
+        Console.WriteLine(f4.GetFractionString());   
+        Console.WriteLine(f4.GetDecimalValue());     
+
+        
+        Fraction f5 = new Fraction(2, 5);
+        Console.WriteLine("\nOriginal f5: " + f5.GetFractionString());
+
+        
+        f5.SetTop(7);
+        f5.SetBottom(8);
+
+        
+        Console.WriteLine("Updated f5: " + f5.GetFractionString());     
+        Console.WriteLine("Updated f5 Decimal: " + f5.GetDecimalValue()); 
     }
 }
---
+++ week03/Fractions/fractions.cs
@@ -0,0 +1,64 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    // Constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getters and Setters
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int value)
+    {
+        _top = value;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int value)
+    {
+        if (value != 0)   // protect against divide by zero
+        {
+            _bottom = value;
+        }
+        else
+        {
+            Console.WriteLine("Denominator cannot be zero. Keeping old value.");
+        }
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
